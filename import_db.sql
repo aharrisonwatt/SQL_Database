41595b8df56bbddbcc150882550e60151808a2fc
App Academy Student <student@appacademy.io>
2016-03-22 12:02:43 -0700

    made questions database

--- import_db.sql
+++ import_db.sql
@@ -1,7 +1,7 @@
 CREATE TABLE users (
   id INTEGER PRIMARY KEY,
   fname VARCHAR(255) NOT NULL,
-  lname VARCHAR(255) NOT NULL,
+  lname VARCHAR(255) NOT NULL
 );
 
 CREATE TABLE questions (
@@ -12,12 +12,11 @@ CREATE TABLE questions (
   FOREIGN KEY (author_id) REFERENCES users(id)
 );
 
-#what is the correct style code for placing foeign key
 CREATE TABLE question_follows (
   question_id INTEGER NOT NULL,
   user_id INTEGER NOT NULL,
   FOREIGN KEY (question_id) REFERENCES questions(id),
-  FOREIGN KEY (user_id) REFERENCES users(id),
+  FOREIGN KEY (user_id) REFERENCES users(id)
 );
 
 CREATE TABLE replies (
@@ -41,17 +40,24 @@ CREATE TABLE question_likes (
 INSERT INTO
   users (fname, lname)
 VALUES
-  ('F1', 'L1'), ('F2', 'L2'), ('F3', 'L3'), ('F4', 'L4')
+  ('F1', 'L1'), ('F2', 'L2'), ('F3', 'L3'), ('F4', 'L4');
 
 INSERT INTO
   questions (title, body, author_id)
 VALUES
-  ('T1', 'B1', 1), ('T2', 'B2', 2), ('T3', 'B3', 3), ('T4', 'B4', 4)
+  ('T1', 'B1', 1), ('T2', 'B2', 2), ('T3', 'B3', 3), ('T4', 'B4', 4);
 
 INSERT INTO
   question_follows (question_id, user_id)
+VALUES
+  (1, 1), (2, 2), (3, 3), (4, 4);
 
 INSERT INTO
   replies (subject_question_id, parent_reply_id, replier_id, body)
 VALUES
-  (1, NULL, 1, 'BR1'), (2, 1, 2, 'BR2'), (3, NULL, 3, 'BR3'), (4, NULL, 4, 'BR4')
+  (1, NULL, 1, 'BR1'), (2, 1, 2, 'BR2'), (3, NULL, 3, 'BR3'), (4, NULL, 4, 'BR4');
+
+INSERT INTO
+  question_likes (liker_id, question_id)
+VALUES
+  (1, 1), (2, 2), (3, 3), (4, 4);
